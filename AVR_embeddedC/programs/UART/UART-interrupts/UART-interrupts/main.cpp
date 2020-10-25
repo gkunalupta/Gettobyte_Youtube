5c409327348ad1f497b34d83b027d0254af7707d
Kunal <kunalguptakew@gmail.com>
2020-10-26 03:15:25 +0530

    Uart-interrupt

--- AVR_embeddedC/programs/UART/UART-interrupts/UART-interrupts/main.cpp
+++ AVR_embeddedC/programs/UART/UART-interrupts/UART-interrupts/main.cpp
@@ -6,10 +6,27 @@
  */ 
 
 #include <avr/io.h>
+#include "uarts.h"
+char rx_byte;
+ISR(USART0_RX_vect)
+{
 
+
+	// We only ever get here when an RXC interrupt has occurred;
+	// Therefore, we know that there must be at least 1 byte in UDR
+
+	// get the 1st byte
+	rx_byte = UDR0;
+    UART_TxChar0(rx_byte);
+
+
+}
 
 int main(void)
 {
+	UART_Init0();
+	printString0("kunal\n");
+	sei();
     /* Replace with your application code */
     while (1) 
     {
