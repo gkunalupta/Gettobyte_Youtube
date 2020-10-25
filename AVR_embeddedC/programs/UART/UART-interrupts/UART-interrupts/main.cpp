3c1cebc6c3d3445aacc8fee1a8ca53812d76dcfd
Kunal <kunalguptakew@gmail.com>
2020-10-25 23:07:22 +0530

    created empty repo

---
+++ AVR_embeddedC/programs/UART/UART-interrupts/UART-interrupts/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * UART-interrupts.cpp
+ *
+ * Created: 25-10-2020 23:06:19
+ * Author : kunal
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
